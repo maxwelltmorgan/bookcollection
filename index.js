dca0d3bc13efd2423f1206cb0cf40f4e34683732
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-09-17 01:57:54 -0600

    firebase refactor

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+var config = {
+    apiKey: "AIzaSyCALA0uHFAXYtudIfa3onw1MDyC0zzHzMw",
+    authDomain: "library-ebc41.firebaseapp.com",
+    databaseURL: "https://library-ebc41.firebaseio.com",
+    projectId: "library-ebc41",
+    storageBucket: "library-ebc41.appspot.com",
+    messagingSenderId: "761388914779"
+};
+firebase.initializeApp(config);
+
+const booksRef = firebase.database().ref('books');
+
+booksRef.once("value").then(function(snapshot) {
+snapshot.forEach(function(childSnapshot) {
+  var key = childSnapshot.key;
+  var childData = childSnapshot.val();
+  var title_val = childSnapshot.val().title;
+  var author_val = childSnapshot.val().author;
+  var pages_val = childSnapshot.val().pages;
+  var read_val = childSnapshot.val().read;
+
+  let bookRow = document.createElement("tr")
+  const bookprop = [title_val,author_val,pages_val,read_val]
+  for (let i = 0; i < bookprop.length; i++) {
+      let bookCell = document.createElement("td")
+      bookCell.innerHTML = [bookprop[i]]
+      bookRow.append(bookCell)
+  }
+
+  let readToggle = document.createElement("a")
+  readToggle.classList.add("readtoggle")
+  readToggle.innerHTML = "toggle status"
+
+  function toggleRead() {
+    read_val = (read_val == "unread" ? "read" : "unread")
+    booksRef.child(key).update( {read: read_val})
+  }
+
+  readToggle.addEventListener("click", function() { toggleRead(read_val) })
+  bookRow.append(readToggle)
+  collection.append(bookRow)
+
+  let removeButton = document.createElement("a")
+  removeButton.classList.add("remove")
+  removeButton.innerHTML = "remove"
+
+  function removeFromLibrary() {
+    booksRef.child(key).remove();
+  }
+
+  removeButton.addEventListener("click", function() { removeFromLibrary(key) })
+  bookRow.append(removeButton)
+  collection.append(bookRow)
+  });
+});
+
 const collection = document.getElementById("collection")
 const toggle  = document.getElementById("toggle");
 const content = document.getElementById("content");
@@ -5,72 +61,24 @@ const newTitle = document.getElementById("new-title");
 const newAuthor = document.getElementById("new-author");
 const newPages = document.getElementById("new-pages");
 const newRead = document.getElementById("new-read");
-const submitButton = document.getElementById("submit-button");
+const submitButton = document.getElementById("submit");
 
-
-let myLibrary = []
-const bookProperties = ["title", "author", "pages", "read"]
-
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+function saveBook(title, author, pages, read){
+    const newBookRef = booksRef.push();
+    newBookRef.set({
+        title: title,
+        author: author,
+        pages: pages,
+        read: read
+    });
 }
 
 function addToLibrary() {
-    let newBook = new Book(newTitle.value, newAuthor.value, newPages.value, newRead.checked ? "read" : "unread")
-    myLibrary.push(newBook)
+    saveBook(newTitle.value, newAuthor.value, newPages.value, newRead.checked ? "read" : "unread")
     newTitle.value = '';
     newAuthor.value = '';
     newPages.value = '';
     newRead.checked = false;
-    render()
 }
-
-function removeFromLibrary(index) {
-  myLibrary.splice(index, 1)
-  render()
-}
-
-function toggleRead(index) {
-  myLibrary[index].read = (myLibrary[index].read == "unread" ? "read" : "unread")
-  render()
-}
-
-function render() {
-    collection.innerHTML = ""
-
-        myLibrary.forEach(function(book, index) {
-
-            let bookRow = document.createElement("tr")
-
-                for (let i = 0; i < bookProperties.length; i++) {
-
-                let bookCell = document.createElement("td")
-
-                bookCell.innerHTML = book[bookProperties[i]]
-                bookRow.append(bookCell)
-                }
-
-                let readToggle =document.createElement("a")
-                readToggle.classList.add("readtoggle")
-
-                readToggle.innerHTML = "toggle status"
-                readToggle.addEventListener("click", function() { toggleRead(index) })
-                bookRow.append(readToggle)
-                collection.append(bookRow)
-
-                let removeButton = document.createElement("a")
-                removeButton.classList.add("remove")
-
-                removeButton.innerHTML = "remove"
-                removeButton.addEventListener("click", function() { removeFromLibrary(index) })
-                bookRow.append(removeButton)
-                collection.append(bookRow)
-        });
-}
-
-render()
 
 toggle.addEventListener("click", function() {content.classList.toggle("show");});
